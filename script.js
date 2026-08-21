ff883edc7e6a90b7d7e552936159b75c2f49ea4f
thetese <thetesemuragije@gmail.com>
2026-08-21 16:37:41 +0200

    refactor: remove dynamic logo injection

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const brandStyles = document.createElement('link');
-brandStyles.rel = 'stylesheet';
-brandStyles.href = 'brand.css';
-document.head.appendChild(brandStyles);
-
-const headerBrand = document.querySelector('.site-header .brand');
-if (headerBrand) {
-  headerBrand.classList.add('brand--official');
-  headerBrand.innerHTML = '<img class="brand-logo" src="/assets/buhanga-logo.svg" alt="Buhanga Eco Lodge">';
-}
-
-const footerBrand = document.querySelector('.footer-brand');
-if (footerBrand) {
-  footerBrand.classList.add('footer-brand--official');
-  footerBrand.innerHTML = '<img class="footer-logo" src="/assets/buhanga-logo.svg" alt="Buhanga Eco Lodge">';
-}
-
 const menuButton = document.querySelector('.menu-button');
 const mobilePanel = document.querySelector('.mobile-panel');
 const mobileLinks = document.querySelectorAll('.mobile-panel a');
